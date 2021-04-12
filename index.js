1dbb68f222eb6d4ef2da244671049d74483067b4
mmengspv <supavet.a@ku.th>
2021-04-12 17:15:05 +0700

    modify index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-import {replyAirQuality} from './service/replyAirQuality.js'
+import { replyAirQuality } from "./service/replyAirQuality.js";
 import { replyMessage } from "./service/reply.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,13 +8,13 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/webhook", async (req, res) => {
-  const {message} = req.body.events[0];
+app.post("/callback", async (req, res) => {
+  const { message } = req.body.events[0];
   let replyToken = req.body.events[0].replyToken;
   if (message.type === "location") {
-    await replyAirQuality({replyToken, message});
-  }else if (message.type === "text") {
-    await replyMessage({replyToken, message});
+    await replyAirQuality({ replyToken, message });
+  } else if (message.type === "text") {
+    await replyMessage({ replyToken, message });
   }
   res.sendStatus(200);
 });
